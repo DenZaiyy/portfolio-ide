6a9ee3c7adbd980b73ad458b57b461590bb25003
DenZaiyy <denzaiyy@outlook.fr>
2023-10-06 02:19:39 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './pages/Home/Home.jsx'
-import Projects from './pages/Projects/Projects.jsx'
-import Contact from './pages/Contact/Contact.jsx'
+import Home from './pages/Home/Home'
+import Projects from './pages/Projects/Projects'
+import Contact from './pages/Contact/Contact'
 import './index.css'
-import Layout from "./pages/Layout/Layout.jsx";
+import Layout from "./pages/Layout/Layout";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import About from "./pages/About/About.jsx";
+import About from "./pages/About/About";
+import NotFound from "./pages/NotFound/NotFound.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Layout/>}>
-					<Route index path="/home" element={<Home/>}/>
+					<Route index path="/" element={<Home/>}/>
 					<Route path="/about" element={<About/>}/>
 					<Route path="/projects" element={<Projects/>}/>
 					<Route path="/contact" element={<Contact/>}/>
+					<Route path="*" element={<NotFound />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
