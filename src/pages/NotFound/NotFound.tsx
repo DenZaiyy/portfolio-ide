0404fab674e6dc168bfcd745824f994fbd0f1bb9
DenZaiyy <denzaiyy@outlook.fr>
2024-01-07 11:00:28 +0100

    feat: add temporary 404 page if route does not exist

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,8 +1,14 @@
+import {Link} from "react-router-dom";
+import "../../assets/styles/variables.css"
+
 const NotFound = () => {
 	return (
-		<>
-			<h1>404</h1>
-		</>
+		<div className="flex flex-col gap-10 items-center justify-center w-full">
+			<h1>_404</h1>
+			<p>// The page you are looking for does not exist.</p>
+			<p>// Please check the url or back to home</p>
+			<Link to="/" className="text-white border-[1px] border-[#FEA55F] hover:underline p-3 bg-[#FEA55F] hover:border-white rounded duration-300">_homepage</Link>
+		</div>
 	);
 }
 export default NotFound;
