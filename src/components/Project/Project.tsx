ee917b8aff41fc72740c2daa6df5ca6e63eb34d1
DenZaiyy <denzaiyy@outlook.fr>
2024-03-08 10:49:04 +0100

    feat: add framer motion for project card

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,85 +1,112 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import DefaultButton from "../Button/Default.tsx";
-import {ProjectInterface} from "../../utils/interfaces.ts"
+import { ProjectInterface } from "../../utils/interfaces.ts";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Project = (props: ProjectInterface) => {
+  const getCollabInfos = async (name: string) => {
+    const res = await fetch(`https://api.github.com/users/${name}`);
+    const data = await res.json();
 
-	const getCollabInfos = async (name: string) => {
-		const res = await fetch(`https://api.github.com/users/${name}`);
-		const data = await res.json();
+    const link = data.html_url;
+    const avatar_url = data.avatar_url;
 
-		const link = data.html_url;
-		const avatar_url = data.avatar_url;
+    return { link, avatar_url };
+  };
 
-		return {link, avatar_url};
-	};
+  const [collabData, setCollabData] = useState<
+    { link: string; avatar_url: string }[]
+  >([]);
 
-	const [collabData, setCollabData] = useState<{ link: string; avatar_url: string }[]>([]);
+  useEffect(() => {
+    const fetchCollaboratorData = async () => {
+      if (props?.collaborators) {
+        const promises = props.collaborators.map(async (collaborator) => {
+          return await getCollabInfos(collaborator.name);
+        });
 
-	useEffect(() => {
-		const fetchCollaboratorData = async () => {
-			if (props?.collaborators) {
-				const promises = props.collaborators.map(async (collaborator) => {
-					return await getCollabInfos(collaborator.name);
-				});
+        const resolvedData = await Promise.all(promises);
+        setCollabData(resolvedData);
+      }
+    };
 
-				const resolvedData = await Promise.all(promises);
-				setCollabData(resolvedData);
-			}
-		};
+    fetchCollaboratorData();
+  }, [props?.collaborators]);
 
-		fetchCollaboratorData();
-	}, [props?.collaborators]);
-
-	return (<div className="text-white flex flex-wrap max-w-md opacity-0">
-		<div className="title flex gap-2 mb-4">
-			<span className="text-[#5565E8]">Project {props.index + 1}</span><span
-			className="text-[#607B96]">// _{props.name}</span>
-		</div>
-		<div
-			className="relative flex flex-col rounded-[15px] bg-[#011221] text-[#607B96] border-[1px] border-[#1E2D3D] min-h-[350px] overflow-hidden">
-			<div className="image max-h-[150px] shrink-0 overflow-hidden">
-				<img src={props.image ? props.image : "/project_default.webp"} alt={`preview for ${props.name} project`} className="object-fill hover:scale-105 duration-200"/>
-			</div>
-			<div className="techno">
-				{props.languages.slice(0, 1).map((language, i) => {
-					return (<div
-						className={`absolute top-2 right-2 z-10 p-1 bg-white/[0.15] backdrop-blur drop-shadow border-[1px] border-blue/40 rounded cursor-pointer`}
-						key={i}>
-						{language.projectIcon}
-					</div>)
-				})}
-			</div>
-			<div
-				className="description border-[#1E2D3D] border-t-[1px] flex flex-col flex-grow items-center justify-evenly gap-5 px-6 py-3">
-				<p className="text-justify text-base">{props.description}</p>
-				<div className="buttons w-full flex flex-row justify-evenly">
-					{props?.preview && <DefaultButton link={props.preview} text="view-project"/>}
-					{props.code && <DefaultButton link={props.code} text="view-source"/>}
-				</div>
-
-			</div>
-			{props?.collaborators && <div className="collaborators py-[10px] px-[14px] border-t-[1px] border-[#1E2D3D]">
-				<div className="title text-[#607B96] text-center mb-2">Collaborators</div>
-				<div className="list flex flex-row gap-x-2 mt-2 items-center justify-center">
-					{collabData.map((collabInfo, i) => (<a
-						href={collabInfo.link}
-						className="flex flex-col gap-y-1 items-center justify-center"
-						target="_blank"
-						rel="noopener noreferrer"
-						key={i}
-					>
-						<img
-							src={collabInfo.avatar_url}
-							alt={`avatar for collaborator ${i + 1}`}
-							className="rounded-full w-[50px] h-[50px] object-fill"
-						/>
-						<span className="text-[#607B96]">{props?.collaborators?.[i].name}</span>
-					</a>))}
-				</div>
-			</div>}
-		</div>
-	</div>);
+  return (
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        className="flex max-w-md flex-wrap text-white"
+      >
+        <div className="title mb-4 flex gap-2">
+          <span className="text-[#5565E8]">Project {props.index + 1}</span>
+          <span className="text-[#607B96]">// _{props.name}</span>
+        </div>
+        <div className="relative flex min-h-[350px] flex-col overflow-hidden rounded-[15px] border-[1px] border-[#1E2D3D] bg-[#011221] text-[#607B96]">
+          <div className="image max-h-[150px] shrink-0 overflow-hidden">
+            <img
+              src={props.image ? props.image : "/project_default.webp"}
+              alt={`preview for ${props.name} project`}
+              className="object-fill duration-200 hover:scale-105"
+            />
+          </div>
+          <div className="techno">
+            {props.languages.slice(0, 1).map((language, i) => {
+              return (
+                <div
+                  className={`border-blue/40 absolute right-2 top-2 z-10 cursor-pointer rounded border-[1px] bg-white/[0.15] p-1 drop-shadow backdrop-blur`}
+                  key={i}
+                >
+                  {language.projectIcon}
+                </div>
+              );
+            })}
+          </div>
+          <div className="description flex flex-grow flex-col items-center justify-evenly gap-5 border-t-[1px] border-[#1E2D3D] px-6 py-3">
+            <p className="text-justify text-base">{props.description}</p>
+            <div className="buttons flex w-full flex-row justify-evenly">
+              {props?.preview && (
+                <DefaultButton link={props.preview} text="view-project" />
+              )}
+              {props.code && (
+                <DefaultButton link={props.code} text="view-source" />
+              )}
+            </div>
+          </div>
+          {props?.collaborators && (
+            <div className="collaborators border-t-[1px] border-[#1E2D3D] px-[14px] py-[10px]">
+              <div className="title mb-2 text-center text-[#607B96]">
+                Collaborators
+              </div>
+              <div className="list mt-2 flex flex-row items-center justify-center gap-x-2">
+                {collabData.map((collabInfo, i) => (
+                  <a
+                    href={collabInfo.link}
+                    className="flex flex-col items-center justify-center gap-y-1"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    key={i}
+                  >
+                    <img
+                      src={collabInfo.avatar_url}
+                      alt={`avatar for collaborator ${i + 1}`}
+                      className="h-[50px] w-[50px] rounded-full object-fill"
+                    />
+                    <span className="text-[#607B96]">
+                      {props?.collaborators?.[i].name}
+                    </span>
+                  </a>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+      </motion.div>
+    </AnimatePresence>
+  );
 };
 
 export default Project;
--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,204 +1,278 @@
 // CSS
-import "./Projects.css";
+import { JSX, useEffect, useRef, useState } from "react";
+import {
+  RiArrowDownSFill,
+  RiArrowRightSFill,
+  RiJavascriptFill,
+} from "react-icons/ri";
 import "../../assets/styles/styles.css";
-import {RiArrowDownSFill, RiArrowRightSFill, RiJavascriptFill} from "react-icons/ri";
-import {JSX, useEffect, useRef, useState} from "react";
-import Tab from "../../components/Tabs/tab.tsx";
 import Project from "../../components/Project/Project.tsx";
+import Tab from "../../components/Tabs/tab.tsx";
+import "./Projects.css";
 
 //icons
-import {FaNodeJs, FaReact, FaSymfony} from "react-icons/fa";
-import {SiPhp} from "react-icons/si";
-import gsap from "gsap";
+// import gsap from "gsap";
+import { FaNodeJs, FaReact, FaSymfony } from "react-icons/fa";
+import { SiPhp } from "react-icons/si";
 
 //images
-import portfolioReact from "/portfolio-react.webp";
-import kGaming from "/k-gaming.webp";
-import forumMVC from "/forum-mvc.webp";
 import brasserieImhoff from "/brasserie-imhoff.webp";
+import forumMVC from "/forum-mvc.webp";
+import kGaming from "/k-gaming.webp";
 import landingPage from "/landing-page.webp";
+import portfolioReact from "/portfolio-react.webp";
 
 //interfaces
-import {LanguageInterface, ProjectsInterface} from "../../utils/interfaces.ts"
-
+import { AnimatePresence, motion } from "framer-motion";
+import {
+  LanguageInterface,
+  ProjectsInterface,
+} from "../../utils/interfaces.ts";
 
 const Projects = () => {
+  const languages: LanguageInterface[] = [
+    {
+      name: "JS",
+      icon: (
+        <RiJavascriptFill
+          color="#607B96"
+          size="1.5em"
+          title="Javascript icon"
+        />
+      ),
+      projectIcon: (
+        <RiJavascriptFill color="#F0DB4F" size="2em" title="Javascript icon" />
+      ),
+    },
+    {
+      name: "PHP",
+      icon: <SiPhp color="#607B96" size="1.5em" title="Php icon" />,
+      projectIcon: <SiPhp color="#777BB3" size="2em" title="Php icon" />,
+    },
+    {
+      name: "React",
+      icon: <FaReact color="#607B96" size="1.5em" title="React icon" />,
+      projectIcon: <FaReact color="#61DBFB" size="2em" title="React icon" />,
+    },
+    {
+      name: "Symfony",
+      icon: <FaSymfony color="#607B96" size="1.5em" title="Symfony icon" />,
+      projectIcon: (
+        <FaSymfony size="2em" color="#000000" title="Symfony icon" />
+      ),
+    },
+    {
+      name: "NodeJS",
+      icon: <FaNodeJs color="#607B96" size="1.5em" title="NodeJS icon" />,
+      projectIcon: <FaNodeJs color="#3C873A" size="2em" title="NodeJS icon" />,
+    },
+  ];
 
-	const languages: LanguageInterface[] = [{
-		name: "JS",
-		icon: <RiJavascriptFill color="#607B96" size="1.5em" title="Javascript icon"/>,
-		projectIcon: <RiJavascriptFill color="#F0DB4F" size="2em" title="Javascript icon"/>
-	}, {
-		name: "PHP", icon: <SiPhp color="#607B96" size="1.5em" title="Php icon"/>, projectIcon: <SiPhp color="#777BB3" size="2em" title="Php icon"/>
-	}, {
-		name: "React", icon: <FaReact color="#607B96" size="1.5em" title="React icon"/>, projectIcon: <FaReact color="#61DBFB" size="2em" title="React icon"/>
-	}, {
-		name: "Symfony", icon: <FaSymfony color="#607B96" size="1.5em" title="Symfony icon"/>, projectIcon: <FaSymfony size="2em" color="#000000" title="Symfony icon"/>
-	}, {
-		name: "NodeJS", icon: <FaNodeJs color="#607B96" size="1.5em" title="NodeJS icon"/>, projectIcon: <FaNodeJs color="#3C873A" size="2em" title="NodeJS icon"/>
-	}]
+  const projectsArr: ProjectsInterface[] = [
+    {
+      name: "portfolio-react",
+      description: "My first portfolio made with React and TailwindCSS",
+      image: portfolioReact,
+      languages: [languages[2]],
+      code: "https://github.com/DenZaiyy/Portfolio-React",
+      preview: "https://k-grischko.fr/",
+    },
+    {
+      name: "k-gaming",
+      description:
+        "E-commerce website for sell video games developed with Symfony 6.2 and PHP 8.2",
+      image: kGaming,
+      languages: [languages[3]],
+      code: "https://github.com/DenZaiyy/K-Gaming",
+      preview: "https://k-gaming.k-grischko.fr/",
+    },
+    {
+      name: "forum-mvc",
+      description:
+        "Forum developed with PHP 8.2 and MVC architecture with a custom framework",
+      image: forumMVC,
+      languages: [languages[1]],
+      code: "https://github.com/DenZaiyy/SQL_Forum",
+      preview: "https://forum.k-grischko.fr/",
+    },
+    {
+      name: "session",
+      description: "Small project to learn how to use Symfony",
+      image: "",
+      languages: [languages[3]],
+      code: "https://github.com/DenZaiyy/sfSessionTW",
+      preview: "https://session.k-grischko.fr/",
+    },
+    {
+      name: "brasserie-imhoff",
+      description:
+        "Website made with HTML CSS and JS for commercial student project",
+      image: brasserieImhoff,
+      languages: [languages[0]],
+      code: "https://github.com/DenZaiyy/BrasserieIMHOFF",
+      preview: "https://brasserie-imhoff.netlify.app/",
+    },
+    {
+      name: "landing-page",
+      description: "Website made with HTML CSS and JS to manipulate DOM",
+      image: landingPage,
+      languages: [languages[0]],
+      code: "https://github.com/DenZaiyy/Landing-page",
+      preview: "https://landing-page-denz.netlify.app/",
+    },
+    {
+      name: "discord-bot",
+      description:
+        "Discord BOT made on collaboration with @Cirec-Coder, for this we used NodeJS and DiscordJS",
+      image: "",
+      languages: [languages[4]],
+      code: "https://github.com/DenZaiyy/cirec_bot",
+      collaborators: [
+        {
+          name: "Cirec-Coder",
+        },
+      ],
+    },
+  ];
 
-	const projectsArr: ProjectsInterface[] = [{
-		name: "portfolio-react",
-		description: "My first portfolio made with React and TailwindCSS",
-		image: portfolioReact,
-		languages: [languages[2]],
-		code: "https://github.com/DenZaiyy/Portfolio-React",
-		preview: "https://k-grischko.fr/"
-	}, {
-		name: "k-gaming",
-		description: "E-commerce website for sell video games developed with Symfony 6.2 and PHP 8.2",
-		image: kGaming,
-		languages: [languages[3]],
-		code: "https://github.com/DenZaiyy/K-Gaming",
-		preview: "https://k-gaming.k-grischko.fr/"
-	}, {
-		name: "forum-mvc",
-		description: "Forum developed with PHP 8.2 and MVC architecture with a custom framework",
-		image: forumMVC,
-		languages: [languages[1]],
-		code: "https://github.com/DenZaiyy/SQL_Forum",
-		preview: "https://forum.k-grischko.fr/"
-	}, {
-		name: "session",
-		description: "Small project to learn how to use Symfony",
-		image: "",
-		languages: [languages[3]],
-		code: "https://github.com/DenZaiyy/sfSessionTW",
-		preview: "https://session.k-grischko.fr/"
-	}, {
-		name: "brasserie-imhoff",
-		description: "Website made with HTML CSS and JS for commercial student project",
-		image: brasserieImhoff,
-		languages: [languages[0]],
-		code: "https://github.com/DenZaiyy/BrasserieIMHOFF",
-		preview: "https://brasserie-imhoff.netlify.app/"
-	}, {
-		name: "landing-page",
-		description: "Website made with HTML CSS and JS to manipulate DOM",
-		image: landingPage,
-		languages: [languages[0]],
-		code: "https://github.com/DenZaiyy/Landing-page",
-		preview: "https://landing-page-denz.netlify.app/"
-	}, {
-		name: "discord-bot",
-		description: "Discord BOT made on collaboration with @Cirec-Coder, for this we used NodeJS and DiscordJS",
-		image: "",
-		languages: [languages[4]],
-		code: "https://github.com/DenZaiyy/cirec_bot",
-		collaborators: [{
-			name: "Cirec-Coder"
-		}]
-	},]
+  function getChecked() {
+    const checkedLang = document.querySelectorAll(
+      "input[type=checkbox]:checked",
+    );
 
-	function getChecked() {
-		const checkedLang = document.querySelectorAll("input[type=checkbox]:checked")
+    return Array.from(checkedLang)
+      .filter((lang) => lang.id !== "faq1" && lang.id !== "faq2")
+      .map((l) => l.id)
+      .join("; ");
+  }
 
-		return Array.from(checkedLang).filter((lang) => lang.id !== "faq1" && lang.id !== "faq2").map((l) => l.id).join("; ")
-	}
+  // const [check, setCheck] = useState( false)
 
-	// const [check, setCheck] = useState( false)
+  const [checkedLanguages, setCheckedLanguages] = useState<
+    Record<string, boolean | JSX.Element>
+  >({});
 
-	const [checkedLanguages, setCheckedLanguages] = useState<Record<string, boolean | JSX.Element>>({});
+  const [filteredProjects, setFilteredProjects] = useState<ProjectsInterface[]>(
+    [],
+  );
 
-	const handleChange = (langName: string, langIcon: JSX.Element) => {
-		setCheckedLanguages((prevCheckedLanguages) => ({
-			...prevCheckedLanguages, [langName]: !prevCheckedLanguages[langName], langIcon: langIcon,
-		}));
-	};
+  // Function to filter projects based on checked languages
+  const filterProjects = () => {
+    const filtered = projectsArr.filter((project) =>
+      project.languages.some((lang) => checkedLanguages[lang.name]),
+    );
+    setFilteredProjects(filtered);
+  };
 
-	const containerRef = useRef<HTMLDivElement>(null);
+  // useEffect to filter projects whenever checkedLanguages changes
+  useEffect(() => {
+    filterProjects();
+  }, [checkedLanguages]);
 
-	useEffect(() => {
-		const container = containerRef.current;
+  const handleChange = (langName: string, langIcon: JSX.Element) => {
+    setCheckedLanguages((prevCheckedLanguages) => ({
+      ...prevCheckedLanguages,
+      [langName]: !prevCheckedLanguages[langName],
+      langIcon: langIcon,
+    }));
+  };
 
-		if (container) {
-			const projectsArray = Array.from(container.children) as HTMLDivElement[];
-			if (projectsArray.length > 0) {
-				gsap.to(projectsArray, {
-					opacity: 1, duration: 1.5, stagger: 0.2,
-				})
-			}
-		}
-	}, [checkedLanguages]);
+  const containerRef = useRef<HTMLDivElement>(null);
 
-	return (<div className="projects">
-		<section className="menu">
-			<ul className="tabs text-white">
-				<li className="item">
-					<input
-						type="checkbox"
-						id="faq1"
-						className="peer appearance-none"
-						defaultChecked={true}
-					/>
-					<div className="title border-b-[1px]">
-						<RiArrowRightSFill className="peer-checked:hidden inline"/>
-						<RiArrowDownSFill className="peer-checked:inline hidden"/>
-						<label
-							htmlFor="faq1"
-							className="py-2 cursor-pointer grow"
-						>
-							projects
-						</label>
-					</div>
-					<div
-						className="peer-checked:max-h-max peer-checked:p-3 peer-checked:border-b-[1px] basis-full border-[var(--lines)] max-h-0 overflow-hidden transition-all duration-100 select-text">
-						<ul className="gap-y-2">
-							{languages.map((lang, i) => (<li className="gap-x-6 items-center cursor-pointer" key={i}>
-								<input
-									type="checkbox"
-									className={`relative cursor-pointer duration-200 peer appearance-none w-[18px] h-[18px] border-[1px] border-blue bg-dark-blue rounded-sm focus:stroke-[2px] focus:stroke-stroke-color focus:border-[#607B96] checked:border-0 checked:bg-[#607B96] checked:text-white`}
-									onChange={() => handleChange(lang.name, lang.icon)}
-									value={lang.name}
-									name={lang.name}
-									id={lang.name}
-								/>
-								<svg
-									className="absolute w-[18px] h-[18px] hidden peer-checked:block pointer-events-none"
-									xmlns="http://www.w3.org/2000/svg"
-									viewBox="0 0 25 25"
-									fill="none"
-									stroke="currentColor"
-									strokeWidth="2"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-								>
-									<polyline points="20 6 9 17 4 12"></polyline>
-								</svg>
-								<label htmlFor={lang.name}
-								       className={`flex cursor-pointer items-center gap-x-2 duration-200 ${checkedLanguages[lang.name] ? 'opacity-100 text-white' : 'opacity-40 text-[#607B96]'}`}>
-									{lang.icon}
-									{lang.name}
-								</label>
-							</li>))}
-						</ul>
-					</div>
-				</li>
-			</ul>
-		</section>
+  return (
+    <div className="projects">
+      <section className="menu">
+        <ul className="tabs text-white">
+          <li className="item">
+            <input
+              type="checkbox"
+              id="faq1"
+              className="peer appearance-none"
+              defaultChecked={true}
+            />
+            <div className="title border-b-[1px]">
+              <RiArrowRightSFill className="inline peer-checked:hidden" />
+              <RiArrowDownSFill className="hidden peer-checked:inline" />
+              <label htmlFor="faq1" className="grow cursor-pointer py-2">
+                projects
+              </label>
+            </div>
+            <div className="max-h-0 basis-full select-text overflow-hidden border-[var(--lines)] transition-all duration-100 peer-checked:max-h-max peer-checked:border-b-[1px] peer-checked:p-3">
+              <ul className="gap-y-2">
+                {languages.map((lang, i) => (
+                  <li className="cursor-pointer items-center gap-x-6" key={i}>
+                    <input
+                      type="checkbox"
+                      className={`border-blue bg-dark-blue focus:stroke-stroke-color peer relative h-[18px] w-[18px] cursor-pointer appearance-none rounded-sm border-[1px] duration-200 checked:border-0 checked:bg-[#607B96] checked:text-white focus:border-[#607B96] focus:stroke-[2px]`}
+                      onChange={() => handleChange(lang.name, lang.icon)}
+                      value={lang.name}
+                      name={lang.name}
+                      id={lang.name}
+                    />
+                    <svg
+                      className="pointer-events-none absolute hidden h-[18px] w-[18px] peer-checked:block"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 25 25"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <polyline points="20 6 9 17 4 12"></polyline>
+                    </svg>
+                    <label
+                      htmlFor={lang.name}
+                      className={`flex cursor-pointer items-center gap-x-2 duration-200 ${checkedLanguages[lang.name] ? "text-white opacity-100" : "text-[#607B96] opacity-40"}`}
+                    >
+                      {lang.icon}
+                      {lang.name}
+                    </label>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </section>
 
-		<section className="content-section">
-			<div className="content !border-r-0">
-				<div className="tab">
-					<Tab name={getChecked() || "nothing selected"}/>
-				</div>
-				<div className="tab-content flex flex-wrap gap-10 h-full items-center justify-center" ref={containerRef}>
-					{projectsArr.filter((project) => project.languages.some((lang) => checkedLanguages[lang.name])).map((project, i) => (<Project
-						name={project.name}
-						description={project?.description}
-						image={project?.image}
-						languages={project.languages}
-						code={project.code}
-						preview={project?.preview}
-						collaborators={project?.collaborators}
-						index={i}
-						key={i}
-					/>))}
-				</div>
-			</div>
-		</section>
-	</div>);
+      <section className="content-section">
+        <div className="content !border-r-0">
+          <div className="tab">
+            <Tab name={getChecked() || "nothing selected"} />
+          </div>
+          <div
+            className="tab-content flex h-full flex-wrap items-center justify-center gap-10"
+            ref={containerRef}
+          >
+            <AnimatePresence>
+              {filteredProjects.map((project, i) => (
+                <motion.div
+                  key={i}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.2, delay: i * 0.2 }}
+                >
+                  <Project
+                    name={project.name}
+                    description={project?.description}
+                    image={project?.image}
+                    languages={project.languages}
+                    code={project.code}
+                    preview={project?.preview}
+                    collaborators={project?.collaborators}
+                    index={i}
+                    key={i}
+                  />
+                </motion.div>
+              ))}
+            </AnimatePresence>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Projects;
