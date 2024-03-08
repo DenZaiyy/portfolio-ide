024ec291d4d3ed27465feafaa5778fc033a47593
DenZaiyy <denzaiyy@outlook.fr>
2024-03-08 12:26:45 +0100

    format all config files in root project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme  : {
-		fontFamily: {
-			mono: ['"Fira Code"', "monospace"],
-		},
-		extend : {
-			colors: {
-				'blue': '#607B96',
-				'dark-blue': "#011627",
-				'stroke-color': 'rgba(96, 123, 150, 0.30)'
-			}
-		},
-	},
-	plugins: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      mono: ['"Fira Code"', "monospace"],
+    },
+    extend: {
+      colors: {
+        blue: "#607B96",
+        "dark-blue": "#011627",
+        "stroke-color": "rgba(96, 123, 150, 0.30)",
+      },
+    },
+  },
+  plugins: [],
 };
